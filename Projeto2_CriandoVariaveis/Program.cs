d06bd1bd98140d2f6d16b1e37d3d34d5e9af1e02
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2022-02-21 15:56:03 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2_CriandoVariaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projeto2_CriandoVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+            int idade;
+            idade = 32;
+
+            Console.WriteLine("Excução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
